03f85376ec981f0b454c6aa3b0ec097ee94aec56
Laura-LopezCasado <47453593+Laura-LopezCasado@users.noreply.github.com>
2019-03-07 20:09:22 +0100

    Tercer commit
    Añado la clase Baraja, que creada a partir de la clase carta.

---
+++ src/clases/Baraja.java
@@ -0,0 +1,37 @@
+package clases;
+
+import java.util.Arrays;
+
+public class Baraja {
+	private Carta[] baraja;
+
+	public Baraja() {
+		int i, j, k = 0;
+		
+		baraja = new Carta[40];
+		
+		for(i = 1; i < 4; i++) {
+			for(j = 1; j < 10; j++) {
+				baraja[k] = new Carta(Valor.values()[j], Palo.values()[i]);
+				k++;
+			}
+		}
+	}
+	
+	public Carta[] getBaraja() {
+		return baraja;
+	}
+	
+	public Carta getCarta(int pos) {
+		return baraja[pos];
+	}
+	
+	public void borrarCarta(int pos) {
+		baraja[pos] = null;
+	}
+
+	@Override
+	public String toString() {
+		return "Baraja [baraja=" + Arrays.toString(baraja) + "]";
+	}
+}

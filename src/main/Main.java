4066515163d1718c69cc3e1c0313a63eb9d66746
Pabloblo <47459121+Pabloblo@users.noreply.github.com>
2019-03-07 20:30:05 +0100

    Ultimisimo commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,22 +4,59 @@ import java.util.Scanner;
 
 import clases.Jugador;
 
-/**
- * <h1>Siete y media</h1>
- * Siete y media es un juego de cartas por turnos en el cual los jugadores
- * tratan de obtener la máxima puntuacion sin pasarse de 7.5.
- * 
- * @author Pablo Rojas
- * @author Laura
- * @version 1.0
- * @since 2019-02-13
- */
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		
+		Scanner S = new Scanner(System.in);
+		
+		System.out.println("Introduce el numero de jugadores, ten en cuenta que uno de ellos será la banca: ");
+		Jugador[] jugadores = new Jugador[S.nextInt()];
+		S.nextLine();
+		for(int i = 0; i < jugadores.length; i++) {
+			jugadores[i] = new Jugador();
+		}
+		
+	
+	
+	
+		S.close();
+	}
+	
+	
+	public static boolean seguirJugando(Jugador[] jugadores) {
+		int cont = 0;
+		boolean seguirJugando = true;
+		for(int i = 0; i < jugadores.length; i++) {
+			if(jugadores[i].isJugando() == false) {
+				cont++;
+			}
+		}
+		if(cont == jugadores.length) {
+			seguirJugando = false;
+		}
+		return seguirJugando;
+		
+	}
 
+	public static Jugador jugadorMaxPuntos(Jugador[] jugadores) {
+		double aux = 1000;
+		int ganador = jugadores.length;
+		
+		for(int i = 0; i < jugadores.length; i++) {
+			if(jugadores[i].getPuntos() < 7.5 && Math.abs(jugadores[i].getPuntos() - 7.5) < aux) {
+				aux = jugadores[i].getPuntos();
+				ganador = i;
+			}
+			else if(Math.abs(jugadores[i].getPuntos() - 7.5) == aux && ganador > i) {
+				aux = jugadores[i].getPuntos();
+				ganador = i; 
+			}
+		}
+		
+		return jugadores[ganador];
 	}
 	
 }
